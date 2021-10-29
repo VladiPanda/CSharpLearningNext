ac45a9ae8f3af02e0d3753720b9c39dd9db41abb
Vladimir Trofimov <trofimov57@mail.ru>
2021-10-29 17:34:01 +0300

    LIFO/FIFO

---
+++ Main/Queue_01/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections;
+
+namespace Queue_01
+{
+    class Program
+    {
+        static void Main()
+        {
+            var queue = new Queue();
+            queue.Enqueue("An item");           // Enqueue() - добавляет элемент в конец очереди.
+            Console.WriteLine(queue.Dequeue()); // Dequeue() - возвращает первый элемент очереди, удаляя его.
+
+            // Delay.
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Main/SessionsCollection/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+/* Создайте коллекцию, в которой бы хранились наименования 12 месяцев, порядковый номер и 
+количество дней в соответствующем месяце. Реализуйте возможность выбора месяцев, как по 
+порядковому номеру, так и количеству дней в месяце, при этом результатом может быть не 
+только один месяц */
+
+namespace SessionsCollection
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<DateTime, List<int>> Months;
+            Months.Add(DateTime.DaysInMonth(2021, 1), 1);
+            
+        }
+    }
+}
