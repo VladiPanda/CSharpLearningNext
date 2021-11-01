cce76f10f9eb5671371b6a4d62e8b030f11448d4
Vladimir Trofimov <trofimov57@mail.ru>
2021-11-01 14:13:21 +0200

    Specialized collection

---
+++ Main/BitArray/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace BitArray
+{
+    class Program
+    {
+        static void Main()
+        {
+            System.Collections.BitArray bits = new System.Collections.BitArray(3);
+            bits[0] = false;
+            bits[1] = true;
+            bits[2] = false;
+            Console.WriteLine(bits.Length);
+
+            // Для изменения размера необходимо воспользоваться свойством Length.
+            bits.Length = 4;
+            bits[3] = true;
+            Console.WriteLine(bits.Length);
+
+            // Delay.
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Main/Hashtable_03/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections;
+
+namespace Hashtable_03
+{
+    class Program
+    {
+        static void Main()
+        {
+            var duplicates = new Hashtable();
+
+            duplicates["First"] = "1st";
+            duplicates["First"] = "the first";
+
+            Console.WriteLine(duplicates.Count);
+
+            // Delay.
+            Console.ReadKey();
+        }
+    }
+}
