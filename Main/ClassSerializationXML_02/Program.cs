7262373a5b2cb56138cec3cb01fe62eae7ffd16d
Vladimir Trofimov <trofimov57@mail.ru>
2021-11-05 20:41:27 +0200

    Serialization adds

---
+++ Main/ClassSerializationXML_02/Program.cs
@@ -0,0 +1,36 @@
+﻿using System.Data;
+using System.IO;
+
+namespace ClassSerializationXML_02
+{
+    class Program
+    {
+        public static void SerializeDataSet(string fileName)
+        {
+            // Создаем DataSet; добавляем таблицу, столбец и десять строк.
+            DataSet dataSet = new DataSet("myDataSet");
+            DataTable table = new DataTable("table1");
+            DataColumn column = new DataColumn("thing");
+
+            table.Columns.Add(column);
+            dataSet.Tables.Add(table);
+
+            DataRow row;
+
+            for (int i = 0; i < 10; i++)
+            {
+                row = table.NewRow();
+                row[0] = "Thing " + i;
+                table.Rows.Add(row);
+            }
+
+            dataSet.WriteXml(fileName);
+            dataSet.WriteXmlSchema(Path.ChangeExtension(fileName, "xsd"));
+        }
+
+        static void Main()
+        {
+            SerializeDataSet("SerializedDataSet.xml");
+        }
+    }
+}
